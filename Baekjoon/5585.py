4ea23f37d54a0defcffeee71fd69e226ffb4f1b1
Cho Yeon Joo <wormjoo@naver.com>
2021-05-21 23:14:30 +0900

    Baekjoon code

---
+++ Baekjoon/5585.py
@@ -0,0 +1,43 @@
+/*
+## 문제
+타로는 자주 JOI잡화점에서 물건을 산다. JOI잡화점에는 잔돈으로 500엔, 100엔, 50엔, 10엔, 5엔, 1엔이 충분히 있고, 언제나 거스름돈 개수가 가장 적게 잔돈을 준다. 
+타로가 JOI잡화점에서 물건을 사고 카운터에서 1000엔 지폐를 한장 냈을 때, 받을 잔돈에 포함된 잔돈의 개수를 구하는 프로그램을 작성하시오.
+예를 들어 입력된 예1의 경우에는 아래 그림에서 처럼 4개를 출력해야 한다.
+![https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/problem/5585/1.png](https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/problem/5585/1.png)
+
+## 입력
+입력은 한줄로 이루어져있고, 타로가 지불할 돈(1 이상 1000미만의 정수) 1개가 쓰여져있다.
+
+## 출력
+제출할 출력 파일은 1행으로만 되어 있다. 잔돈에 포함된 매수를 출력하시오.
+*/
+
+money = int(input())
+change = 1000-money
+count = 0
+
+while(change >= 500):
+    change = change - 500
+    count+=1
+
+while(change >= 100):
+    change = change - 100
+    count+=1
+
+while(change >= 50):
+    change = change - 50
+    count+=1
+
+while(change >= 10):
+    change = change - 10
+    count+=1
+
+while(change >= 5):
+    change = change - 5
+    count+=1
+
+while(change >= 1):
+    change = change - 1
+    count+=1
+
+print(count)
