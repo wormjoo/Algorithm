3c5a8bd73450a4bf78b527ded7c53d687348bca0
wormjoo <wormjoo@naver.com>
2022-04-28 23:09:25 +0900

    Problem Solving

---
+++ Baekjoon/2839.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+d = [5001] * (n + 1)
+d[0] = 0
+array = [3, 5]
+
+for i in range(2):
+    for j in range(array[i], n + 1):
+        if d[j - array[i]] != 5001:
+            d[j] = min(d[j], d[j - array[i]] + 1)
+    
+if d[n] == 5001:
+    print(-1)
+else:
+    print(d[n])
