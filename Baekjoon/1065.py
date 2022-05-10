a0455560eac484e4f79bba84796483f740963e53
wormjoo <wormjoo@naver.com>
2022-05-10 23:42:33 +0900

    Problem Solving

---
+++ Baekjoon/1065.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = 0
+
+for i in range(1, n + 1):
+    if i <= 99:
+        cnt += 1
+    else:
+        num = list(map(int, str(i)))
+        if num[0] - num[1] == num[1] - num[2]:
+            cnt += 1
+
+print(cnt)
