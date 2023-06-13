e041bf9fa43a21aa95d44982e00c3119a43a2582
wormjoo <wormjoo@naver.com>
2023-06-13 20:48:01 +0900

    2023.06.13

---
+++ Baekjoon/11053.py
@@ -0,0 +1,11 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+d = [1] * n
+
+for i in range(1, n):
+    for j in range(0, i):
+        if A[j] < A[i]:
+            d[i] = max(d[i], d[j] + 1)
+
+print(max(d))
